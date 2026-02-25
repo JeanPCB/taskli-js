909d93a08224f8faede5719e52f7d7a219d03121
JeanPCB <92955454+JeanPCB@users.noreply.github.com>
2026-02-25 14:42:27 -0300

    Implements read-write tasks file
    Ensure that the tasks has a mocked data file
    created by standard

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 #!/usr/bin/env node
+import { readFileSync, writeFileSync, existsSync } from "node:fs";
+
+// read-write guarantee
+const TASKS_FILE = "./taskList.js";
+
+function ensureStorageFile(tasksFile) {
+  if (existsSync(tasksFile)) {
+    const readTasks = readFileSync(tasksFile, "utf-8");
+  } else {
+    writeFileSync(tasksFile, JSON.stringify([]));
+  }
+}
+
+function main() {
+  ensureStorageFile(TASKS_FILE);
+}
+
+main();
